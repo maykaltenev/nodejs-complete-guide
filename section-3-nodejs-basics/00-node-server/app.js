009d496fefb8695fb34f8b09457638805a9fb31c
Maykal Tenev <michaeltenev@gmail.com>
2023-04-10 21:38:37 +0200

    node-js

--- section-3-nodejs-basics/00-node-server/app.js
+++ section-3-nodejs-basics/00-node-server/app.js
@@ -2,12 +2,11 @@ const http = require("http");
 
 
 const server = http.createServer((req, res) => {
-
     console.log(req.url, req.method, req.headers);
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>My First Page</title><head>");
-    res.write("<body><h1>Hello from My Node.js Server!</title><head>");
+    res.write("<body<h1>Hello from My Node.js Server!</title><head>");
     res.write("</html>")
     res.end();
 
