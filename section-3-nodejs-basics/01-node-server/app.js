be9343ccb0b46cc23f1327f940d8dda40c8e3e7a
Maykal Tenev <michaeltenev@gmail.com>
2023-04-10 20:19:55 +0200

    write and setHeader

--- section-3-nodejs-basics/01-node-server/app.js
+++ section-3-nodejs-basics/01-node-server/app.js
@@ -2,7 +2,15 @@ const http = require("http");
 
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+
+    console.log(req.url, req.method, req.headers);
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title><head>");
+    res.write("<body><h1>Hello from My Node.js Server!</title><head>");
+    res.write("</html>")
+    res.end();
+
 });
 
 server.listen(3000);
